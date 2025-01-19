9befe6c524f8d65207f4dc75667477b670b1d8d6
var-shikhar <shikharvarshney10@gmail.com>
2025-01-19 23:56:27 +0530

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
 import './App.css';
 import BootstrapList from './bootstrapList';
 
-// const listMode = [
-//   {
-//     slug: 'bootstrap',
-//     title: 'Bootstrap List',
-//   },
-//   {
-//     slug: 'normal',
-//     title: 'Normal List',
-//   }
-// ]
-
 function App() {
-
   return <BootstrapList />
 }
-
-
-{/* 
-  const [activeMode, setActiveMode] = useState(listMode[0].slug);
-  
-  <div>Select List Mode</div>
-      <div className='d-flex gap-2'>
-        {listMode?.map(item => <Button key={item.slug} onClick={() => setActiveMode(item.slug)}>{item.title}</Button>)}
-      </div>
-      {activeMode === 'Bootstrap' ? <BootstrapList /> : activeMode === 'Bootstrap' ? <NormalList /> : null } */}
 
 export default App
